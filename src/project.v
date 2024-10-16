7303d9817cd67b9e8d2d56f2de2fc7f98a95be4e
galihat <103354074+galihat@users.noreply.github.com>
2024-10-16 21:41:11 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_inv-ringosci (
+module tt_um_inv_ringosci (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
